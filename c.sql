71aa034bb6d4b3d7d6a72d5cdecd923bc642fe82
ruchitdesai007 <70881647+ruchitdesai007@users.noreply.github.com>
2023-12-04 17:28:50 +0530

    Update c.sql

--- c.sql
+++ c.sql
@@ -1,28 +1,30 @@
 WITH PlayerStrikeRates AS (
     SELECT
-        batter,
-        COUNT(*) AS total_runs,
-        SUM(runs_batter) AS total_runs_scored,
-        COUNT(DISTINCT d.match_id) AS total_matches,
-        ROUND(SUM(runs_batter) * 100.0 / COUNT(*), 2) AS strike_rate
+        p.player_name,
+        ROUND((SUM(d.runs_batter) * 100.0) / NULLIF(SUM(d.balls_faced), 0), 2) AS strike_rate
     FROM
         deliveries d
     JOIN
         innings i ON d.inning_id = i.inning_id
     JOIN
         matches m ON i.match_id = m.match_id
+    JOIN
+        players p ON d.batter = p.player_name
     WHERE
-        d.runs_batter > 0
-        AND m.season = '2019'
+        m.year = '2019'
+        AND d.bye_runs = 0
+        AND d.legbye_runs = 0
+        AND d.wide_runs = 0
+        AND d.noball_runs = 0
     GROUP BY
-        batter
+        p.player_name
 )
 
 SELECT
-    batter,
+    player_name,
     strike_rate
 FROM
     PlayerStrikeRates
 ORDER BY
     strike_rate DESC
-LIMIT 10; -- Adjust the limit based on the number of top players you want to retrieve
+LIMIT 10; 
